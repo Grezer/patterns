928e920d30bf92acc1d22fdfa10097f9a0fc0032
RzaevNR_notebook <rzaevnr@list.ru>
2021-01-12 21:52:59 +0300

    feat: start project

---
+++ my_painter/my_painter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("my_painter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("my_painter")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("118cee3b-4d36-4ffb-a0f7-d3329fe2348f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ vectorPainter/vectorPainter/Figure.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vectorPainter
+{
+    public abstract class Figure
+    {
+        // Basic figure parameters
+        private float private_xAxis;
+        private float private_yAxis;
+        private float private_width;
+        private float private_height;
+
+
+        // maybe rewrite this to public float xAxis { get { return private_xAxis; } } ???
+        public float xAxis => private_xAxis;
+        public float yAxis => private_yAxis;
+        public float width => private_width;
+        public float height => private_height;
+
+
+        // Move figure => change the private values ​​of the x and y axes
+        public virtual void Move(float new_xAxis, float new_yAxis)
+        {
+            private_xAxis = new_xAxis;
+            private_yAxis = new_yAxis;
+        }
+
+
+        // Resize figure => change the private values ​​of the width and height
+        public virtual void Resize(float new_width, float new_height)
+        {
+            private_width = new_width;
+            private_height = new_height;
+        }
+
+        // Draw figure => abstract method that inheritors must implement 
+        public abstract void Draw(Graphics g);
+    }
+}
